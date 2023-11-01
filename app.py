4ab9132b76220353fa3cf067fcd6076ee0183c71
Rekitonei <isnainipribadi@gmail.com>
2023-11-01 09:15:42 +0700

    login dan logout 1/11/2023

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 app = Flask(__name__)
 db = SQL("sqlite:///score.db")
-udb = SQL("sqlite:///user.db")
 app.config.update(SECRET_KEY=os.urandom(24))
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -43,35 +42,53 @@ def register():
     if request.method == "POST":
         if not request.form.get("Username"):
             return "must provide username"
-        elif not request.form.get("Passwrod"):
+        elif not request.form.get("Password"):
             return "must provide password"
-        rows = udb.execute("SELECT * FROM user WHERE Username = ?", request.form.get("Username"))
+        rows = db.execute("SELECT * FROM user WHERE Username = ?", request.form.get("Username"))
 
         email = request.form.get("Email")
         name_user = request.form.get("Name")
         username = request.form.get("Username")
         password = request.form.get("Password")
-        password_repeat = request.get("rpassword")
-
-        print(email)
-        print(name_user)
-        print(username)
-        print(password)
+        password_repeat = request.form.get("rpassword")
 
         hash = generate_password_hash(password)
         if len(rows) == 1:
             return "username already taken"
         if password == password_repeat:
-            udb.execute("INSERT INTO user (username, hash) VALUES(?, ?)", username, hash)
+            db.execute("INSERT INTO user (username, password, email, name) VALUES(?, ?, ?, ?)", username, hash, email, name_user)
 
-            registered_user = udb.execute("select * from user where username = ?", username)
+            registered_user = db.execute("select * from user where username = ?", username)
             session["id"] = registered_user[0]["id"]
             flash("you were sucessfully registered")
             return redirect("/")
-
         else:
-
             return render_template("register.html", userdb=userdb)
-        
     else:
         return render_template("register.html")
+
+@app.route("/login",methods=['POST', 'GET'])
+def login():
+    """Log user in"""
+    session.clear()
+    if request.method == "POST":
+        if not request.form.get("Username"):
+            return "must provide username"
+        elif not request.form.get("Password"):
+            return "must provide password"
+        rows = db.execute("SELECT * FROM user WHERE username = ?",request.form.get("Username"))
+        if len(rows) != 1 or not check_password_hash(rows[0]["password"],request.form.get("Password")):
+            return "invalid username and/or password"
+        
+        session["user_id"] = rows[0]["id"]
+        return redirect("/")
+    else:
+        return render_template("login.html")    
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+    session.clear()
+    flash("berhasil logout")
+    return redirect("/")
